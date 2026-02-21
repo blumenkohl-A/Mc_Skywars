62aa95448ba4dc6c591d6175ea7d6d92175c1630
Alex <alexkohl@deas-hits.de>
2026-02-21 15:32:49 +0100

    0.2

--- src/main/java/de/skywars/command/SkyWarsCommand.java
+++ src/main/java/de/skywars/command/SkyWarsCommand.java
@@ -4,9 +4,7 @@ import de.skywars.game.GameManager;
 import de.skywars.game.GameState;
 import de.skywars.island.IslandManager;
 import de.skywars.team.SkyTeam;
-import net.kyori.adventure.text.Component;
-import net.kyori.adventure.text.format.NamedTextColor;
-import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
@@ -33,13 +31,25 @@ public class SkyWarsCommand implements CommandExecutor, TabCompleter {
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
         
         String name = command.getName().toLowerCase();
+        
+        // Konsolen-Log zur Diagnose
+        Bukkit.getLogger().info("[SkyWars-Debug] Befehl empfangen: /" + label + " " + String.join(" ", args));
 
-        // 1. Check Shortcuts
-        if (name.equals("setlobby")) return handleSetLobby(sender);
-        if (name.equals("setisland")) return handleSetIsland(sender, args);
-        if (name.equals("swstart")) return handleStart(sender);
+        // 1. Shortcuts
+        if (name.equals("setlobby")) {
+            handleSetLobby(sender);
+            return true;
+        }
+        if (name.equals("setisland")) {
+            handleSetIsland(sender, args);
+            return true;
+        }
+        if (name.equals("swstart")) {
+            handleStart(sender);
+            return true;
+        }
 
-        // 2. Handle /skywars <subcommand>
+        // 2. Hauptbefehl /skywars
         if (args.length == 0) {
             sendHelp(sender);
             return true;
@@ -51,10 +61,7 @@ public class SkyWarsCommand implements CommandExecutor, TabCompleter {
                 msg(sender, "§6[SkyWars] §eStatus: §f" + gameManager.getState());
                 msg(sender, "§6[SkyWars] §eSpieler: §f" + gameManager.getAlivePlayers().size());
             }
-            case "debug" -> {
-                msg(sender, "§a[SkyWars] Plugin ist aktiv und reagiert!");
-                msg(sender, "§7Version: 1.0.0 | JDK 21+ Build");
-            }
+            case "debug" -> msg(sender, "§a[SkyWars] Plugin Version 1.0.2 ist aktiv!");
             case "setlobby" -> handleSetLobby(sender);
             case "setisland" -> handleSetIsland(sender, shift(args));
             case "start" -> handleStart(sender);
@@ -65,68 +72,62 @@ public class SkyWarsCommand implements CommandExecutor, TabCompleter {
         return true;
     }
 
-    private boolean handleSetLobby(CommandSender sender) {
+    private void handleSetLobby(CommandSender sender) {
         if (!sender.hasPermission("skywars.admin")) {
-            msg(sender, "§cKeine Berechtigung (skywars.admin).");
-            return true;
+            msg(sender, "§cKeine Rechte (skywars.admin).");
+            return;
         }
         if (!(sender instanceof Player p)) {
-            msg(sender, "§cNur Spieler koennen Spawns setzen.");
-            return true;
+            msg(sender, "§cNur fuer Spieler.");
+            return;
         }
         islandManager.setLobbySpawn(p.getLocation());
-        msg(sender, "§a[SkyWars] Lobby-Spawn erfolgreich gesetzt!");
-        return true;
+        msg(sender, "§a[SkyWars] Lobby-Spawn gesetzt!");
+        Bukkit.getLogger().info("[SkyWars] Lobby gesetzt von " + p.getName());
     }
 
-    private boolean handleSetIsland(CommandSender sender, String[] args) {
+    private void handleSetIsland(CommandSender sender, String[] args) {
         if (!sender.hasPermission("skywars.admin")) {
-            msg(sender, "§cKeine Berechtigung.");
-            return true;
+            msg(sender, "§cKeine Rechte.");
+            return;
         }
         if (!(sender instanceof Player p)) {
-            msg(sender, "§cNur Spieler.");
-            return true;
+            msg(sender, "§cNur fuer Spieler.");
+            return;
         }
         if (args.length < 1) {
-            msg(sender, "§cBenutzung: /skywars setisland <red|blue|green|yellow>");
-            return true;
+            msg(sender, "§cNutze: /setisland <red|blue|green|yellow>");
+            return;
         }
         SkyTeam team = SkyTeam.fromString(args[0]);
         if (team == null) {
-            msg(sender, "§cTeam '" + args[0] + "' nicht gefunden.");
-            return true;
+            msg(sender, "§cTeam '" + args[0] + "' existiert nicht.");
+            return;
         }
         islandManager.setTeamSpawn(team, p.getLocation());
         msg(sender, "§a[SkyWars] Spawn fuer Team " + team.getColoredName() + " §agesetzt!");
-        return true;
     }
 
-    private boolean handleStart(CommandSender sender) {
-        if (!sender.hasPermission("skywars.admin")) return true;
-        if (gameManager.getState() == GameState.RUNNING) {
-            msg(sender, "§cDas Spiel lauft bereits.");
-            return true;
-        }
+    private void handleStart(CommandSender sender) {
+        if (!sender.hasPermission("skywars.admin")) return;
+        msg(sender, "§eVersuche Spiel zu starten...");
         gameManager.startGame();
-        return true;
     }
 
-    private boolean handleStop(CommandSender sender) {
-        if (!sender.hasPermission("skywars.admin")) return true;
+    private void handleStop(CommandSender sender) {
+        if (!sender.hasPermission("skywars.admin")) return;
         gameManager.stopGame();
-        msg(sender, "§eSpiel wurde abgebrochen.");
-        return true;
+        msg(sender, "§cSpiel abgebrochen.");
     }
 
     private void msg(CommandSender sender, String text) {
-        sender.sendMessage(LegacyComponentSerializer.legacySection().deserialize(text));
+        // Nutze die klassische Methode zur Sicherheit
+        sender.sendMessage(text);
     }
 
     private void sendHelp(CommandSender sender) {
-        msg(sender, "§6--- SkyWars Hilfe ---");
+        msg(sender, "§6--- SkyWars Befehle ---");
         msg(sender, "§e/sw info §7- Status");
-        msg(sender, "§e/sw debug §7- Test");
         msg(sender, "§e/sw setlobby §7- Lobby setzen");
         msg(sender, "§e/sw setisland <team> §7- Insel setzen");
         msg(sender, "§e/sw start §7- Starten");
@@ -139,19 +140,12 @@ public class SkyWarsCommand implements CommandExecutor, TabCompleter {
 
     @Override
     public List<String> onTabComplete(@NotNull CommandSender sender, @NotNull Command command, @NotNull String alias, @NotNull String[] args) {
-        List<String> completions = new ArrayList<>();
-        String name = command.getName().toLowerCase();
-
-        if (name.equals("skywars") || name.equals("sw")) {
-            if (args.length == 1) {
-                completions.addAll(Arrays.asList("info", "debug", "setlobby", "setisland", "start", "stop"));
-            } else if (args.length == 2 && args[0].equalsIgnoreCase("setisland")) {
-                for (SkyTeam t : SkyTeam.values()) completions.add(t.name().toLowerCase());
-            }
-        } else if (name.equals("setisland") && args.length == 1) {
-            for (SkyTeam t : SkyTeam.values()) completions.add(t.name().toLowerCase());
+        List<String> list = new ArrayList<>();
+        if (args.length == 1) {
+            list.addAll(Arrays.asList("info", "debug", "setlobby", "setisland", "start", "stop"));
+        } else if (args.length == 2 && args[0].equalsIgnoreCase("setisland")) {
+            for (SkyTeam t : SkyTeam.values()) list.add(t.name().toLowerCase());
         }
-
-        return completions.stream().filter(s -> s.startsWith(args[args.length - 1].toLowerCase())).collect(Collectors.toList());
+        return list.stream().filter(s -> s.startsWith(args[args.length - 1].toLowerCase())).collect(Collectors.toList());
     }
 }
